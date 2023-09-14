98668936a5f941a58fe3a9bd808275ad9521e966
Christian <c.ship.johnson@gmail.com>
2023-09-14 15:05:57 -0400

    14SEP2023

--- Lab-2/Homework.c
+++ Lab-2/Homework.c
@@ -48,52 +48,59 @@ Check characters value
 
    
 */
+#include <stdio.h>
 
 int main() {
 
   int Longest_H = 0;
   int Longest_T = 0;
   int CurrentRun = 0;
-  char Winner;
-  
-  while (!feof(stdin)) //Runs while loop while not end of file
-    {
-      char CurrentChar = getchar(); // Gets current Char
-      char PrevChar;
-      if (CurrentChar !='T' && CurrentChar !='H')
-	{continue;} // Skips character if neither H nor T
+  char CurrentChar;
+  char PreviousChar = '\0'; // Initialize PreviousChar to a default value
 
+  //printf("Begin\n"); // Present for debugging. Uncomment to check beginning. 
+  
+  while (!feof(stdin)) // 
+    {
+      CurrentChar = getchar();
+      if (CurrentChar != 'T' && CurrentChar != 'H')
+	{
+	  //printf("Char Not Recognized\n"); // Present for Debugging. Uncomment to check each character individually. 
+	  continue;
+	} // Skips character if neither H nor T
 
       if (CurrentChar == PreviousChar && CurrentChar == 'H')
 	{
 	  CurrentRun++;
-	  if (CurrentRun>Longest_H)
-	    {Longest_H = CurrentRun}
-	} //Checks if we have repetition, and whether its an H. If its repeated, increase the current run length. Checks whether the current run is longer than the longest run, and resets longest H run if so.
+	  if (CurrentRun > Longest_H)
+	    {
+	      Longest_H = CurrentRun;
+	    }
+	  //printf("H, %d\n", CurrentRun); // Present for Debugging. Uncomment to check each character individually. 
+	} // Checks if we have repetition and whether it's an H. If it's repeated, increase the current run length. Checks whether the current run is longer than the longest run and resets the longest H run if so.
+
       if (CurrentChar == PreviousChar && CurrentChar == 'T')
 	{
 	  CurrentRun++;
-	  if (CurrentRun>Longest_T)
-	    {Longest_T = CurrentRun}
-	} //Checks if we have repetition, and whether its an T. If its repeated, increase the current run length. Checks whether the current run is longer than the longest run, and resets longest T run if so.
+	  if (CurrentRun > Longest_T)
+	    {
+	      Longest_T = CurrentRun;
+	    }
+	  //printf("T, %d\n", CurrentRun); // Present for Debugging. Uncomment to check each character individually.
+	} // Checks if we have repetition and whether it's a T. If it's repeated, increase the current run length. Checks whether the current run is longer than the longest run and resets the longest T run if so.
 
-      
       if (CurrentChar != PreviousChar)
 	{
 	  CurrentRun = 1;
-	} //Checks if we have ended the run, and sets the current length to 1. 
+	  //printf("Switch\n"); // Present for Debugging. Uncomment to check each character individually.
+	} // Checks if we have ended the run and sets the current length to 1.
 
-      PreviousChar = CurrentChar; // Sets the previous character before incrementing. 
+      PreviousChar = CurrentChar; // Sets the previous character before incrementing.
     } // End of while loop
 
+  if (Longest_H == Longest_T) {printf("B, %d\n", Longest_H);}
+  if (Longest_T > Longest_H) {printf("T, %d\n", Longest_T);}
+  if (Longest_H > Longest_T) {printf("H, %d\n", Longest_H);}
 
-
-
-
-
-  if (Longest_H == Longest_T) {print B, length}
-  if (Longest_T > Longest_H) {print T, length}
-  if (Longest_H > Longest_T) {print H, length}
-  
-  return(0);
+  return 0;
 }
